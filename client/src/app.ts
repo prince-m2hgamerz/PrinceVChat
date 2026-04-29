1c15f1de072911f1021c23ff5564ce29e4974f21
Prince kumar <prince.m2hgamerz@gmail.com>
2026-04-29 14:52:25 +0530

    Debug: detailed logging for room-users

--- client/src/app.ts
+++ client/src/app.ts
@@ -106,10 +106,13 @@ class App {
       // When we join, we get list of existing users
       this.socketManager.on('room-users', (msg: any) => {
         const users = msg.payload as { id: string; username: string }[];
-        console.log('[App] Got room users:', users);
+        console.log('[App] Got room users:', JSON.stringify(users));
+        console.log('[App] My userId:', this.userId);
         for (const user of users) {
+          console.log('[App] Checking user:', user.id, 'matches?', user.id === this.userId);
           if (user.id !== this.userId) {
             // Add user to UI directly (not via WebRTC)
+            console.log('[App] Adding user:', user.id, user.username);
             this.ui.addUser(user.id, false, user.username);
           }
         }
